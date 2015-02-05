e5a5c5edfd9e079379081cddce3296727fb45e3f
joshuamontage <noreply>
2015-02-05 23:42:48 +0000

    Add component test2

---
+++ ui/test2.reel/test2.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/test2.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Test2
+ * @extends Component
+ */
+exports.Test2 = Component.specialize(/** @lends Test2# */ {
+    constructor: {
+        value: function Test2() {
+            this.super();
+        }
+    }
+});
